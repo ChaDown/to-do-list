39d02adb1a02211d9cfcf3676192a43824909ad1
ChaDown <charliedownes@hotmail.com>
2022-09-14 12:53:48 +0100

    Basic barebones file structure with webpack set-up

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("function ToDoItem(title, description, dueDate, priority) {\n  (this.title = title),\n    (this.description = description),\n    (this.dueDate = dueDate),\n    (this.priority = priority);\n}\n\nconst toDo1 = new ToDoItem(\"a\", \"b\", \"c\", \"d\");\n\nconsole.log(toDo1);\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+function ToDoItem(title, description, dueDate, priority) {
+  (this.title = title),
+    (this.description = description),
+    (this.dueDate = dueDate),
+    (this.priority = priority);
+}
+
+const toDo1 = new ToDoItem("a", "b", "c", "d");
+
+console.log(toDo1);
